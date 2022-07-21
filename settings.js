2713ae3b671ab1b902f089aac20517fb5c5ed728
Yaka modz <109526153+YakaModz@users.noreply.github.com>
2022-07-21 14:03:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,44 +19,44 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is Devi Bot🔥
+My Creator is Samith dilshan😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94742265843'] //Owner number in aive msg
+global.premium = ['+94742265843'] //Owner Number info
+global.ownernomer = '+94742265843' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'Samith Dilshan' //Owner Name
+global.botname = ' Devil Bot' //Bot Name
 
-global.button = '☰ GITHUB ☰' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://github.com/prabhasha2006' //Change Your Button's URL
+global.button = '☰ Youtube ☰' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://youtube.com/channel/UCDc_vYQFGHznliwSypY_5CA' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'Sri Lanka' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
-global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
+global.packname = 'Devil Bot' //Bot Pacage Name
+global.author = 'Samith Dilshan' //Author Name
+global.prefa = ['','!','.','🤣','🥰','🎀'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
-global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.dragonemoji = '🎀 ' // Menu emoji
+global.footer = '©_samith.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://youtube.com/channel/UCDc_vYQFGHznliwSypY_5CA' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
-    success: 'Done ✅',
-    admin: 'Only Admins can use it!😉',
-    botAdmin: 'Bot wants to be an Admin😥',
-    owner: 'Only Owner can use this!😉',
-    group: 'Feature Used Only For Groups!',
-    private: 'This Features Only For Private Chat!',
-    bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
-    error: 'Error!!! Maybe Api Key Is Expired🤔!',
-    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    success: 'හරි ✅',
+    admin: 'පරිපාලකවරු සදහා පමණි!😉',
+    botAdmin: 'මම පරිපාලක නොවේ😥',
+    owner: 'හිමිකරුට පමනි😉',
+    group: 'මෙම විශේෂාංගය ගෲප් සදහා පමණි',
+    private: 'මෙම විශේෂාංගය පෞද්ගලික චැට් සදහා පමණි',
+    bot: 'මෙම විශේෂාංගය බොට් සදහා පමණි😝',
+    wait: '👉👈රැදී සිටින්න...',
+    error: 'දෝශයක්!! සමහර විට api එක කල් පැනල ඇති🤔!',
+    endLimit: 'ඔබ දෛනික ප්‍රමාණය ඉක්මවා ගොස් ඇත.නැවත පැය 12කින් උතසහ කරන්න',
 }
 // DO NOT CHANGE \\
     global.limitawal = {
